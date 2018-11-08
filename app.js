e3e56f1db9610ffb3a4266941a968596833b10ca
zygist87 <zygist87@gmail.com>
2018-11-08 21:30:48 +0200

    is klases

---
+++ app.js
@@ -0,0 +1,49 @@
+console.log("labasss")
+console.log(19+30)
+console.log(5>10)
+var name ="tomas";
+console.log(name);
+name="petras"
+console.log(name);
+
+var manoVardas='zygis';
+var c = 12;
+var f = c*1.8+32;
+console.log(f);
+
+var myName = "zygis";
+
+var myName = "z y g i s";
+
+var text = "i couldn't do my homework because";
+console.log(text);
+var tex = 'i had a "lot to do" at my work!';
+console.log(tex);
+let pasiteisinimas = 'i couldn\'t do my\ni had a lot \"jkljvjv\" at my work';
+console.log(pasiteisinimas);
+
+let pirmas = "Pirmo sakinio tekstas.\n";
+let antras = "Antro sakinio tekstas.\n";
+let trecias = "Trecio sakinio tekstas.\n";
+console.log(pirmas+antras+trecias);
+
+let signedIn;
+console.log(signedIn);
+
+let bill = 10.25+3.99+7.15;
+let tip = 0.15;
+let total = bill*tip
+console.log(total);
+console.log(total.toFixed(2));
+
+let first = "a.\t firstName\n";
+let second = "b.\t Interest\n";
+let third = "c.\t Hobby\n";
+console.log(first+second+third)
+let awesome = "first+second+third";
+console.log(awesome);
+
+prompt('Iveskite vidutini pensijos dydi');
+let biudzetas = 12.5 milijardu euru\n;
+let pensijinisAmziuszmoniu = 637244\n;
+console.log(biudzetas + pensijinisAmziuszmoniu );
